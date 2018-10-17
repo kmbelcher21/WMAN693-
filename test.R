f167703756a58ab9d26998d9546e0d3e31fe6f88
Kirsten Belcher <kmbelcher@mix.wvu.edu>
2018-10-17 09:41:53 -0400

    something

---
+++ test.R
@@ -0,0 +1,25 @@
+#doing addition
+
+5+3
+5+4
+#assigning a value to 'x'
+x<-3
+5+x
+variable=56
+
+variable+x
+y<-'a letter'
+
+class(x)
+class(y)
+
+z <-matrix(data=1:16,ncol=4,nrow=4,
+            byrow=T)
+z
+class(z)
+
+1==1
+
+# high level math
+1+1=2
+
